8470439844d28177eca51053630aeaddd1da2a5d
Gashrod1 <101563672+Gashrod1@users.noreply.github.com>
2024-02-01 10:16:20 +0100

    aaaa

--- python.py/lol.py
+++ python.py/lol.py
@@ -4,3 +4,5 @@ if message[:2].isalpha():
     if message[3] == "-":
         if message[4:7].isdigit():
             print("Le format du message de commit est correct.")
+        else:
+            print("Nop")
